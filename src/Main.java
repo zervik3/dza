06dd5c72d0dc9a5438aa41e063a63fa3c56e8e3c
Gleb <glebsoldakov33@gmail.com>
2024-09-19 19:07:38 +0300

    fdssdfsdfdfssfdfdssdffsdfsdfdsfsd

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,13 @@ import java.lang.reflect.Method;
 
 public class Main {
     public static void main(String[] args) {
-        // Получаем объект класса String
+
         Class<String> stringClass = String.class;
 
-        // Получаем все методы класса String
+
         Method[] methods = stringClass.getDeclaredMethods();
 
-        // Выводим информацию о каждом методе
+
         for (Method method : methods) {
             System.out.println("Метод: " + method.getName());
         }
